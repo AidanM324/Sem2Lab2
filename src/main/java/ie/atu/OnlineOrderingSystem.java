9ae819b7b773f844ce5676e6735f3905d33d4c84
Aidan Malone <G00423306@atu.ie>
2024-02-09 12:07:21 +0000

    Created new Classes Salad and Pizza + fixed some errors in original commit

--- src/main/java/ie/atu/OnlineOrderingSystem.java
+++ src/main/java/ie/atu/OnlineOrderingSystem.java
@@ -1,23 +1,23 @@
 package ie.atu;
 
-import java.sql.SQLOutput;
-
 public class OnlineOrderingSystem {
 
     public static void main(String[] args) {
         //Create an array or list of MenuItem objects
-        Menuitem[] menuitems = new Menuitem[] {
+        MenuItem[] menuitems = new MenuItem[10] {
                 //Add new menu Items
+            new Burger("Classic Burger", 8.99, "Beef Patty, Lettuce, Tomato, Burger Sauce");
+
         };
 
         //display details of each menu item
-        for(Menuitem menuitem : menuitems){
+        for(MenuItem menuitem : menuitems){
             //Print out the Name, description and Price for each item
         }
 
         //Calculate and display the total cost of the order
         double totalCost = 0;
-        for(Menuitem menuitem : menuitems){
+        for(MenuItem menuitem : menuitems){
             //totalCost of all the menuitem prices in an order
         }
         System.out.println("Total Cost: £" + totalCost);
