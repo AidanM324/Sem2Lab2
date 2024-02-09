422a90d431467aee59ffe9e9c3a708706f298581
Aidan Malone <G00423306@atu.ie>
2024-02-09 12:37:41 +0000

    Created new Classes Dessert + fixed Menu print out + total cost print out

--- src/main/java/ie/atu/Dessert.java
+++ src/main/java/ie/atu/Dessert.java
@@ -1,11 +1,11 @@
 package ie.atu;
 
-class Burger implements MenuItem {
+class Dessert implements MenuItem{
     private String name;
     private double price;
     private String ingredients;
 
-    public Burger(String name, double price, String ingredients){
+    public Dessert(String name, double price, String ingredients){
         this.name = name;
         this.price = price;
         this.ingredients = ingredients;
